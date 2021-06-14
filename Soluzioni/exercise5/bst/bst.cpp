78db282454bc966fe610557d845e355bf5fa87db
Antonio Garofalo <antonio.garofalo125@gmail.com>
2021-06-14 14:41:24 +0200

    Vari update libreria 4 + libreria 5 (27).

--- Soluzioni/exercise5/bst/bst.cpp
+++ Soluzioni/exercise5/bst/bst.cpp
@@ -226,7 +226,7 @@ bool BST<Data>::Exists(const Data &dato) const noexcept {
 template <typename Data>
 Data BST<Data>::DataNDelete(struct BST<Data>::NodeLnk *node) {
     Data dato;
-    std::swap(dato,node->Element());
+    std::swap(dato,node->info);
     delete node;
     return dato;
 }
